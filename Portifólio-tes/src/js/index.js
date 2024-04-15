c2f280eed030263bffd2fadcb375092b62bc3bf5
feltonmauro <159842188+feltonmauro@users.noreply.github.com>
2024-04-15 13:12:45 -0300

    Add files via upload

---
+++ Portifólio-tes/src/js/index.js
@@ -0,0 +1,38 @@
+// mensagem de inicio no site
+// alert('oi');
+
+/* 
+    Objetivo 1 - Quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+        1 - Pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+        Passo 2 - Identificar o clique no botão
+        Passo 3 - Adicionar a classe 'ativo' nos projetos escondidos
+
+    Objetivo 2 - Esconder o botão de mostrar mais 
+        Passo 1 - Pegar o botão e esconder ele
+*/
+
+// Objetivo 1 - Quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+
+// Passo 1 - Pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+// Passo 2 - Identificar o clique no botão
+botaoMostrarProjetos.addEventListener('click', () => {
+    // Passo 3 - Adicionar a classe 'ativo' nos projetos escondidos
+    mostrarMaisProjetos();
+
+// Objetivo 2 - Esconder o botão de mostrar mais 
+// Passo 1 - Pegar o botão e esconder ele
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
